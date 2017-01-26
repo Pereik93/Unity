4ac134e3e7e1b568c25b4d64a4bca23904ef9552
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2017-01-26 17:35:25 +0100

    Glitch Garden
    Made the base for the game with some basic animations

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;    
+
+	public void Start(){
+        if (autoLoadNextLevelAfter == 0)
+        {
+            Debug.Log("Level auto load disabled");
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    public void LoadLevel(string name){
+		
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Quit game requested");
+		Application.Quit();
+	}
+
+	
+}
