70e3275255f6cde3dd50b283f895e6ce7a72a4a5
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2017-02-26 00:10:33 +0100

    Zombie Runner
    A Zombie Apocalypse Survival Game.
    Made terrain
    Multiple spawn points
    Helicopter
    Standard unity first person charater

---
+++ Zombie Runner/Assets/Scripts/Eyes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+    private Camera eyes;
+    private float defaulFOV = 70;
+    
+
+	// Use this for initialization
+	void Start () {
+        eyes = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Zoom"))
+        {
+            eyes.fieldOfView = Mathf.Lerp(eyes.fieldOfView, defaulFOV / 1.5f, Time.deltaTime * 3);
+        } else
+        {
+            eyes.fieldOfView = defaulFOV;
+        }
+	}
+}
